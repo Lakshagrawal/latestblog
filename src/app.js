bc963f6d238d851969ee9efc3704e634bac4ca0f
Lakshya Agrawal <lakshya424agrawal@gmail.com>
2024-02-19 18:07:19 +0530

    updating prev and next logic

--- src/app.js
+++ src/app.js
@@ -45,19 +45,11 @@ app.use('/api',auth);
 
 
 // connect to mongoose
-// let dbURL = "mongodb://127.0.0.1:27017/website_tut";
-// mongoose.connect(dbURL,()=>{
-//     console.log("db connect hello my friends ");   
-// })
-// let dbURL = "mongodb://localhost:27017/bloging";
+
 try{
     const dbURL = process.env.SERVER_DB_KEY || "mongodb://localhost:27017/bloging";  
-    console.log(dbURL);
-    // const connectionParams = { 
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true
-    // };
-
+    // console.log(dbURL);
+    
     mongoose.connect(dbURL).then(()=>{
         console.log("db is connected succsesfully");
     }).catch((err)=>{
